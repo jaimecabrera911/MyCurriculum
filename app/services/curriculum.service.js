350f4b6106b33eb4bf58869e817c1c43804294d7
JaimeCabreraDev <cabrerajaimeandres@gmail.com>
2023-12-13 02:48:35 -0500

    Crear Api Curriculum
    En este commit, se implementa la lógica necesaria para realizar consultas a la base de datos MongoDB y recuperar información sobre el curriculum.

--- app/services/curriculum.service.js
+++ app/services/curriculum.service.js
@@ -1,13 +1,16 @@
 import {connectToDatabase,closeDatabaseConnection} from "../db/db.config.js"
 
 import {ObjectId} from "mongodb";
+import {logger} from "../config/logger.js";
 
 export const getCurriculum = async (curriculumId) => {
     const database = await connectToDatabase();
+    logger.info(curriculumId)
     const curriculum = await database
         .db()
         .collection("curriculums")
         .findOne({_id: new ObjectId(curriculumId)});
+    logger.info(curriculum)
     await closeDatabaseConnection();
     return curriculum;
 };
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import {getCurriculum} from "./app/services/curriculum.service.js";
+import { getCurriculum } from "./app/services/curriculum.service.js";
 import logger from "morgan";
+import cors from 'cors'
 
 const app = express();
 
+
 app.use(logger('dev'))
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send({"appName":"MyCurriculum"});
+    res.send({ "appName": "MyCurriculum" });
 });
 
 app.get('/:id', async (req, res) => {
